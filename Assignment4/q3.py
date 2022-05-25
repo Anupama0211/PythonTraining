961ce2f837b61591f3a88edb6e5b26e4d46b0978
Anupama_Jha <Anupama_Jha@epam.com>
2022-05-25 23:19:50 +0530

    Assignment 4 corrections

--- Assignment4/q3.py
+++ Assignment4/q3.py
@@ -6,6 +6,11 @@ and prints all the lines which are longer than 30 characters using generators.
 
 def generator(*filenames):
     for filename in filenames:
-        if len(filename) > 30:
-            yield filename
+        with open(filename) as file:
+            line = file.readline()
+            if (len(line) > 30):
+                yield line
 
+
+for line in generator("file1.txt", "file2.txt"):
+    print(line)
